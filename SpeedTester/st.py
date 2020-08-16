47169fb64b5bde5436a39469b8bb3e88a0da360d
folomeev1980 <Lazarev0205!980>
2020-08-16 20:22:46 +0300

    radiobot_red

--- SpeedTester/st.py
+++ SpeedTester/st.py
@@ -20,22 +20,22 @@ while True:
         driver = webdriver.Chrome("chromedriver.exe", options=options)
         driver.get(url)
         driver.find_element_by_xpath(
-            "/html/body/div[1]/div[1]/div[1]/div[1]/section[2]/div[1]/div[2]/div/button").click()
+            "/html/body/div[1]/div[1]/div[1]/div[1]/section[2]/div[1]/div[2]/div[1]/div/div/div/button").click()
         time.sleep(60)
         response = driver.page_source
         soup = BeautifulSoup(response, "lxml")
-        soup = soup.findAll("div", class_="speed-progress-bar__value")
+        print(soup)
+        soup = soup.find("div", class_="speed-progress-bar__detailed")
         upload = soup[0].text.strip().split("=")[0].strip().split(" ")
-        download = soup[1].text.strip().split("=")[0].strip().split(
-            " ")
+        #download = soup[1].text.strip().split("=")[0].strip().split(" ")
         tm = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         driver.close()
         driver.quit()
-        #print(upload, download)
+        # print(upload, download)
 
         with open('speed_test_home_new.csv', mode='a') as speed_test:
             speed_test = csv.writer(speed_test, delimiter=';', lineterminator='\n')
-            row = (tm, point(upload[0]), point(upload[1]), "   ", point(download[0]), point(download[1]))
+            row = (tm, point(upload[0]), point(upload[1]), "   ",)
             print(row)
             speed_test.writerow(row)
     except Exception as e:
